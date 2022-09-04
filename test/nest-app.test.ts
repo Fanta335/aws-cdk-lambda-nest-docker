e7c75e42c269a7b92cba0d5e76d6c77bb0275538
Fanta335 <fantayusuke5210@gmail.com>
2022-09-04 22:27:23 +0900

    Initial commit

---
+++ test/nest-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NestApp from '../lib/nest-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nest-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NestApp.NestAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
