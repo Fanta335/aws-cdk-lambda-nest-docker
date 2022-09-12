19cabe20e8cca8af20d13bdef3a0d07fa2f19270
Fanta335 <fantayusuke5210@gmail.com>
2022-09-12 23:16:04 +0900

    ルートエンドポイントにhello worldの追加

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -5,6 +5,11 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get()
+  getHello() {
+    return this.appService.getHello();
+  }
+
   @Get('foo')
   foo() {
     return {
