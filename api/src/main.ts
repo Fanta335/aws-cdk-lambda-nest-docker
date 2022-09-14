086cd758358e12ef64e63ce77ace6e12b28f5c44
Fanta335 <fantayusuke5210@gmail.com>
2022-09-14 13:35:00 +0900

    コメント部分を削除

--- api/src/main.ts
+++ api/src/main.ts
@@ -50,44 +50,4 @@ export const handler = async (
     return serverlessExpressInstance(event, context);
   }
   return bootstrapServer(event, context);
-
-  // const params = event.queryStringParameters ? event.queryStringParameters : {};
-
-  // const RESPONSE_HEADERS = {
-  //   'Content-Type': 'application/json',
-  //   'Access-Control-Allow-Origin': '*',
-  //   'Access-Control-Allow-Headers': 'Content-Type,Authorization,access-token',
-  // };
-
-  // const secretsManager = new AWS.SecretsManager({
-  //   region: 'ap-northeast-1',
-  // });
-  // const response = await secretsManager
-  //   .getSecretValue({
-  //     SecretId: 'NestAppStack-rds-credentials',
-  //   })
-  //   .promise();
-
-  // const { host, username, password } = JSON.parse(response.SecretString ?? '');
-
-  // const connection = await mysql.createConnection({
-  //   host: process.env.PROXY_ENDPOINT,
-  //   user: username,
-  //   password,
-  //   database: 'app',
-  //   // ssl: {
-  //   //   cert: fs.readFileSync(__dirname + '/cert/AmazonRootCA1.pem', 'utf-8')
-  //   // },
-  //   ssl: {
-  //     cert: cert,
-  //   },
-  // });
-
-  // const result = await connection.query('select * from user');
-
-  // return {
-  //   statusCode: 200,
-  //   headers: RESPONSE_HEADERS,
-  //   body: JSON.stringify(result[0]),
-  // };
 };
