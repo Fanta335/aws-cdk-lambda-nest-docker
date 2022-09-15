38c0839b405d71d7e1dfaba7babecddbbeb0a693
Fanta335 <fantayusuke5210@gmail.com>
2022-09-15 20:17:39 +0900

    ローカル確認用にデフォルト値を追加

--- api/src/configurations.ts
+++ api/src/configurations.ts
@@ -2,11 +2,11 @@ import * as AWS from 'aws-sdk';
 
 export const getSecretsValuesFromAwsSecretManager = async () => {
   const secretsManager = new AWS.SecretsManager({
-    region: process.env.AWS_REGION,
+    region: process.env.AWS_REGION || 'ap-northeast-1',
   });
   const response = await secretsManager
     .getSecretValue({
-      SecretId: process.env.RDS_SECRET_NAME,
+      SecretId: process.env.RDS_SECRET_NAME || 'NestAppStack-rds-credentials',
     })
     .promise();
 
